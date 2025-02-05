794312982836a214c41d243a0b7970aede6ae6b5
daviidevvvv <davidevalente6@gmail.com>
2025-02-05 21:05:00 +0100

    Add files via upload

---
+++ PHP-Web-Server/service-worker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'workout-app-cache-v1';
+const urlsToCache = [
+    '/',
+    '/styles.css',
+    '/scripts.js',
+    '/index.html',
+    '/moon-icon.png',
+    '/sun-icon.png',
+    // Aggiungi altre risorse statiche qui
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Cache hit - return the cached response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
